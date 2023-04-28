8e0a5c26c9850febe0dd1d8dba37ddb3779037a7
reyhanmichiels <michiels.reyhan@gmail.com>
2023-04-29 02:43:22 +0700

    DAONote interface

---
+++ app/src/main/java/com/example/modul8/database/DAONote.java
@@ -0,0 +1,22 @@
+package com.example.modul8.database;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface NoteDAO {
+    @Insert(onConflict = onConflictStrategy.IGNORE)
+    void insert (Note note);
+    @Update
+    void update (Note note);
+    @Delete
+    void delete (Note note);
+    @Query("SELECT * from note ORDER BY id ASC")
+    LiveData<List<Note>> getAllNotes();
+}
