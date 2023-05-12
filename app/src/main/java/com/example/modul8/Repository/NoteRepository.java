69a83eff3abf11d6408aa53be7d34ea96528e1bb
reyhanmichiels <michiels.reyhan@gmail.com>
2023-05-12 10:38:11 +0700

    create Note Repository Class

--- app/src/main/java/com/example/modul8/Repository/NoteRepository.java
+++ app/src/main/java/com/example/modul8/Repository/NoteRepository.java
@@ -1,5 +1,40 @@
 package com.example.modul8.Repository;
 
-public class NoteRepository {
+import android.app.Application;
 
+import androidx.lifecycle.LiveData;
+
+import com.example.modul8.database.DAONote;
+import com.example.modul8.database.Note;
+import com.example.modul8.database.NoteRoomDB;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class NoteRepository {
+    private final DAONote mDaoNotes;
+    private final ExecutorService executorService;
+
+    public NoteRepository(Application application) {
+        executorService = Executors.newSingleThreadExecutor();
+        NoteRoomDB db = NoteRoomDB.getDatabase(application);
+        mDaoNotes = db.daoNote();
+    }
+
+    public LiveData<List<Note>> getAllNotes() {
+        return mDaoNotes.getAllNotes();
+    }
+
+    public void insert (final Note note) {
+        executorService.execute(() -> mDaoNotes.insert(note));
+    }
+
+    public void delete (final Note note) {
+        executorService.execute(() -> mDaoNotes.delete(note));
+    }
+
+    public void update (final Note note) {
+        executorService.execute(() -> mDaoNotes.update(note));
+    }
 }
