83e52b06ee453d0d03b69ae6d2f47d9b39265c55
reyhanmichiels <michiels.reyhan@gmail.com>
2023-05-12 19:58:29 +0700

    Initialize MainActivity class

--- app/src/main/java/com/example/modul8/ui/MainActivity.java
+++ app/src/main/java/com/example/modul8/ui/MainActivity.java
@@ -1,4 +1,54 @@
 package com.example.modul8.ui;
 
-public class MainActivity {
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.modul8.R;
+import com.example.modul8.databinding.ActivityMainBinding;
+import com.example.modul8.helper.ViewModelFactory;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private NoteAdapter adapter;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        NoteMainViewModel noteMainViewModel = obtainViewModel(MainActivity.this);
+        noteMainViewModel.getAllNotes().observe(this, notes -> {
+            if (notes != null) {
+                adapter.setListNotes(notes);
+            }
+        });
+
+        adapter = new NoteAdapter();
+        binding.rvNotes.setLayoutManager(new LinearLayoutManager(this));
+        binding.rvNotes.setHasFixedSize(true);
+        binding.rvNotes.setAdapter(adapter);
+        binding.fabAdd.setOnClickListener(view -> {
+            if (view.getId() == R.id.fab_add) {
+                Intent intent = new Intent(MainActivity.this, InsertUpdateActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding = null;
+    }
+
+    private static NoteMainViewModel obtainViewModel(AppCompatActivity activity) {
+        ViewModelFactory factory = ViewModelFactory.getInstance(activity.getApplication());
+
+        return new ViewModelProvider(activity, factory).get(NoteMainViewModel.class);
+    }
 }
