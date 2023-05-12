b44d129c9110d43aa96259516a7a0c0f69968742
reyhanmichiels <michiels.reyhan@gmail.com>
2023-05-12 10:28:25 +0700

    fixing bug

--- app/src/main/java/com/example/modul8/database/NoteRoomDB.java
+++ app/src/main/java/com/example/modul8/database/NoteRoomDB.java
@@ -4,9 +4,10 @@ import android.content.Context;
 
 import androidx.room.Database;
 import androidx.room.Room;
+import androidx.room.RoomDatabase;
 
 @Database(entities = {Note.class}, version = 1)
-public abstract class NoteRoomDB {
+public abstract class NoteRoomDB extends RoomDatabase {
     public abstract DAONote daoNote();
 
     public static volatile NoteRoomDB INSTANCE;
@@ -14,7 +15,7 @@ public abstract class NoteRoomDB {
     public static NoteRoomDB getDatabase(final Context context) {
         if (INSTANCE == null) {
             synchronized (NoteRoomDB.class) {
-                INSTANCE Room.databaseBuilder(context.getApplicationContext(), NoteRoomDB.class, "note_db").build();
+                INSTANCE = Room.databaseBuilder(context.getApplicationContext(), NoteRoomDB.class, "note_db").build();
             }
         }
         return INSTANCE;
